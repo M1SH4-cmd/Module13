4f517babb84b1e723a345f0f5f93986afb0c6319
OG_Michael <59878471+M1SH4-cmd@users.noreply.github.com>
2024-11-06 00:23:47 +0700

    Task 2 reworked

--- task_2/src/main.cpp
+++ task_2/src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
+int main() {
+    vector<float> prices {2.5, 4.25, 3.0, 10.0};
 
-int main(){
+    vector<int> items {1, 1, 0, 3};
 
-    vector<float> prices = {2.5, 4.25, 3.0, 10.0};
-    vector<int> items = {1, 1, 0, 3};
-    float sum = 0;
+    float totalCost = 0.0;
 
-    for(int i = 0; i < items.size(); i++){
-        sum += prices[items[i]];
+    for (int index : items) {
+        if (index >= 0 && index < prices.size()) {
+            totalCost += prices[index];
+        }
     }
-    cout << sum << endl;
+
+    cout << "Суммарная стоимость будет равна " << totalCost << "." << endl;
+
+    return 0;
 }
